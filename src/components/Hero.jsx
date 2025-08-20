8855ed87fc2a65d8da5fc5eb61dbde0814d0e849
WajidAli05 <wajidalise@gmail.com>
2025-08-20 16:19:50 +0500

    Updated info of hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,35 +26,35 @@ const Hero = () => {
       ref={heroRef}
       className="hero min-h-screen"
       style={{
-        backgroundImage: "url(/hero.jpg)",
+        backgroundImage: "url(/hero.jpg)"
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
 
       <div className="hero-content text-neutral-content text-center w-full px-6">
         <div className="w-full">
-          <h1
-            className={`mb-6 text-6xl font-extrabold leading-tight opacity-0 ${
+          <h4
+            className={`mb-6 text-6xl font-extrabold leading-tight opacity-0 pt-[100px] ${
               inView ? "animate-fadeUp" : ""
             }`}
           >
-            Welcome To Al Arab Zone
-          </h1>
+            Welcome To House Sparrow Construction & Maintanence And Al Arab Zone Bike Food Delivery Services
+          </h4>
 
           <p
             className={`mb-8 text-lg max-w-4xl mx-auto opacity-0 ${
               inView ? "animate-fadeUp delay-200" : ""
             }`}
           >
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi. Provident cupiditate voluptatem et in. Quaerat fugiat ut
-            assumenda excepturi exercitationem quasi. In deleniti eaque aut
-            repudiandae et a id nisi. Provident cupiditate voluptatem et in.
-            Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
-            deleniti eaque aut repudiandae et a id nisi. Provident cupiditate
-            voluptatem et in. Quaerat fugiat ut assumenda excepturi
-            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+            At House Sparrow, we build more than structures—we build trust. Based in Qatar, 
+            our team specializes in high-quality construction, renovation, and complete 
+            maintenance solutions for homes, offices, and commercial spaces. From foundations 
+            to finishing touches, we ensure durability, modern design, and reliable service
+             tailored to your needs.
+             <br />
+             Alongside construction, we proudly operate Al Arab Zone, a fast and efficient 
+             bike delivery service across Qatar. Whether it’s food, groceries, or essentials, 
+             we deliver with speed, safety, and reliability—making your daily life easier.
           </p>
 
           <button
